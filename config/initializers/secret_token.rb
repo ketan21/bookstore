17df4644b5ef0eb7d385f6e06264c5fa56f4c35f
Ketan Deshmukh <ketan.davvindore@gmail.com>
2012-07-10 16:30:24 +0530

    added spree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookstore::Application.config.secret_token = '2e4c5f7d5af91c0ed0c1e5d421fff09dfe0798ae02e866f7aa275b62857826471070a9f1b1c7a1657cd84d76feb80fb436f6cd3fbe5ad66288354088df88eaac'
